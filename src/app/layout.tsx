3a62804d89cdcdf4b6d6e808b69aca0efa9729e9
JACKSPARROWbts <18tucs221.skct@gmail.com>
2024-04-21 22:40:11 +0530

    Added open graph support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,19 @@ import Head from "next/head";
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pulse Portfolio",
-  description:
-    "My portfolio illustrates how engineering PULSES like a HEARTBEAT within me.",
+  title: "Shankar Raju's Portfolio",
+  description: "My portfolio illustrates how engineering PULSES like a HEARTBEAT within me.",
+  // Add OG meta tags here (optional)
+  openGraph: {
+    title: "Shankar Raju's Portfolio",
+    description: "My portfolio illustrates how engineering PULSES like a HEARTBEAT within me.",
+    type: "website",
+    siteName:"Pulse Portfolio",
+    images:[{
+      url:"https://ogp.dsabyte.com/api/image.png?layoutName=Blog&Title=Welcome+to+my+portfolio&Author+Name=Shankar+Raju&Author+Avtar=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F59025857%3Fv%3D4&Icon+URL=https%3A%2F%2Fdsabyte.com%2Flogo.png&Tags=Data+Scientist%2C+Machine+Learning+Engineer%2C+AI+Engineer&Theme=dark&ext=.png"
+  }]
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,23 +28,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
       <Head>
-        {/* Common Open Graph tags */}
-        <meta property="og:title" content="Shankar Raju's Portfolio" />
-        <meta property="og:description" content="My portfolio illustrates how engineering PULSES like a HEARTBEAT within me" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://ogp.dsabyte.com/api/image.png?layoutName=Blog&Title=Welcome+to+my+portfolio&Author+Name=Shankar+Raju&Author+Avtar=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F59025857%3Fv%3D4&Icon+URL=https%3A%2F%2Fdsabyte.com%2Flogo.png&Tags=Data+Scientist%2C+Machine+Learning+Engineer%2C+AI+Engineer&Theme=dark&ext=.png" />
-
-        {/* Twitter specific meta tags */}
-        <meta name="twitter:card" content="Shankar Raju's Portfolio" />
-        <meta name="twitter:title" content="Shankar Raju's Portfolio" />
-        <meta name="twitter:description" content="My portfolio illustrates how engineering PULSES like a HEARTBEAT within me" />
-        <meta name="twitter:image" content="https://ogp.dsabyte.com/api/image.png?layoutName=Blog&Title=Welcome+to+my+portfolio&Author+Name=Shankar+Raju&Author+Avtar=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F59025857%3Fv%3D4&Icon+URL=https%3A%2F%2Fdsabyte.com%2Flogo.png&Tags=Data+Scientist%2C+Machine+Learning+Engineer%2C+AI+Engineer&Theme=dark&ext=.png" />
-
-        {/* Facebook specific meta tags */}
-        <meta property="og:site_name" content="Pulse Portfolio" />
-        {/* Add any other Facebook-specific meta tags as needed */}
-
-        {/* Favicon and other necessary links */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
